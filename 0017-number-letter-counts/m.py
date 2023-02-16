9ed9d5439f0f3c185504fde12ed5aebabd72602d
Dustin Miao <dutin20.meow@gmail.com>
2023-02-15 18:48:47 -0800

    added problem 17, 19

---
+++ 0017-number-letter-counts/m.py
@@ -0,0 +1,10 @@
+from num2words import num2words
+
+ans = 0
+for i in range(1, 1001):
+	s = num2words(i)
+	print(i, s)
+	ans += len(s.replace(' ', '').replace('-', ''))
+with open('answer.txt', 'w+') as af:
+	print(ans, file = af)
+	af.close()

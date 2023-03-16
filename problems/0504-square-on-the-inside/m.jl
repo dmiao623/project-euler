4ed041bd7a2e8d963faa9cd0894ba6f07c4c9149
Dustin Miao <dutin20.meow@gmail.com>
2023-03-16 08:46:12 -0700

    added problem 504

---
+++ problems/0504-square-on-the-inside/m.jl
@@ -0,0 +1,15 @@
+function solve(m)
+	ans = 0
+	for a in 1:m, b in 1:m, c in 1:m, d in 1:m
+		k::Int32 = (a * b + b * c + c * d + d * a - gcd(a, b) - gcd(b, c) - gcd(c, d) - gcd(d, a)) / 2 + 1
+		s = trunc(Int, sqrt(k) + 0.5)
+		if s ^ 2 == k
+			ans += 1
+		end 
+	end
+	return ans
+end
+
+open("answer.txt", "w") do df
+    write(df, string(solve(100)))
+end

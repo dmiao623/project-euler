3534da5b9006cae5f85e4e71825adf99834738fd
Dustin Miao <dutin20.meow@gmail.com>
2023-03-15 21:04:54 -0700

    added problem 188

---
+++ problems/0188-the-hyperexponentiation-of-a-number/m.jl
@@ -0,0 +1,10 @@
+using Primes
+
+function tetr(a, b, m)
+    if b == 0 return 1 end
+    return powermod(a, tetr(a, b - 1, Primes.totient(m)), m)
+end
+
+open("answer.txt", "w") do df
+    write(df, string(tetr(1777, 1855, 10^8)))
+end

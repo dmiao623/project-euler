41121410339ee0709640b0da4f1c8e43d34795c8
Dustin Miao <dutin20.meow@gmail.com>
2023-03-20 18:19:33 -0700

    added num-prime-factors generator

---
+++ library/generators/num-prime-factors.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+#include "../../util/cmd-line-parser.hpp"
+
+std::vector<int> num_prime_factors(int n) {
+	std::ifstream df("../sequences/least-factor.txt");
+	std::vector<int> lf(n + 1);
+	if (df) {
+		for (int i = 1; i <= n; i++)
+			df >> lf[i];
+		df.close();
+	}
+
+	std::vector<int> f(n + 1);
+	f[1] = 0;
+	for (int i = 2; i <= n; i++) {
+		int k = i / lf[i];
+		f[i] = f[k] + (lf[i] != lf[k]);
+	}
+	return f;
+}
+
+int main(int argc, char *argv[]) {
+	auto stime = std::chrono::high_resolution_clock::now();
+
+	auto _n = get_arg(argv, argv + argc, "-n");
+	int n = _n == "" ? 100'000'000 : std::stoi(_n);
+	std::string fpath = get_arg(argv, argv + argc, "-f");
+	assert(fpath != "");
+	auto _plen = get_arg(argv, argv + argc, "-p");
+	int plen = std::min(n, _plen == "" ? 10 : std::stoi(_plen));
+	
+	std::cout << "terms  : " << n << '\n';
+	std::cout << "file   : " << fpath << '\n';
+
+	auto f = num_prime_factors(n);
+
+	std::ofstream of(fpath);
+	for (int i = 1; i <= n; i++)
+		of << f[i] << '\n';
+	of.close();
+
+	std::cout << "prev   : [";
+	for (int i = 1; i < plen; i++)
+		std::cout << f[i] << (i == plen - 1 ? ", ...]\n" : ", ");
+
+	auto etime = std::chrono::high_resolution_clock::now();
+	auto time = std::chrono::duration_cast<std::chrono::seconds>(etime - stime).count();
+	std::cout << "time   : " << time << " seconds\n";
+}

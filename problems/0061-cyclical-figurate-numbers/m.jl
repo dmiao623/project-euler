36f431af585fa269b64ce4143c29787b757ffab7
Dustin Miao <67491820+dustin-miao@users.noreply.github.com>
2023-06-14 08:49:27 -0700

    added problem 61

--- problems/0061-cyclical-figurate-numbers/m.jl
+++ problems/0061-cyclical-figurate-numbers/m.jl
@@ -1,47 +1,49 @@
-using Combinatorics
-
-function generate(f, ll, rr)
-	r = []
-	for i in 1:rr
-		k = f(i)
-		if k > rr break end
-		if k < ll continue end
-		push!(rr, k)
-	end
-	return rr
-end
-
 function connected(a, b) a % 10 == b ÷ 100 % 10 && a ÷ 10 % 10 == b ÷ 1000 end
 
-function solve(ll, rr)
-	k = 6
+function solve()
+	function generate(f)
+		r = []
+		for i in 1:10000
+			k = f(i)
+			k > 9999 && break
+			k < 1000 && continue
+			push!(r, k)
+		end
+		return r
+	end
+	
 	p₀ = [
-		generate(k -> k * (k + 1) ÷ 2, ll, rr),
-		generate(k -> k * k, ll, rr),
-		generate(k -> k * (3 * k - 1) ÷ 2, ll, rr),
-		generate(k -> k * (2 * k - 1), ll, rr),
-		generate(k -> k * (5 * k - 3) ÷ 2, ll, rr),
-		generate(k -> k * (3 * k - 2), ll, rr)
+		generate(k -> k * (k + 1) ÷ 2),
+		generate(k -> k * k),
+		generate(k -> k * (3 * k - 1) ÷ 2),
+		generate(k -> k * (2 * k - 1)),
+		generate(k -> k * (5 * k - 3) ÷ 2),
+		generate(k -> k * (3 * k - 2))
 	]
 
-	function recurse(a)
-		l = last(a)
-		if length(a) == 6
-			
-		end
-	end
+	answer = Vector{Int}()
 
-	for p ∈ permutations(p₀)
-		Set([])
-		for l ∈ 2:k
-			for i ∈ length(p[l])
-				for j 
+	function recurse(first, last, mask)
+		if mask == 63 return connected(last, first) end
+		for m ∈ 1:6
+			if mask >> (m - 1) & 1 == 0
+				for p ∈ p₀[m]
+					if connected(last, p)
+						ret = recurse(first, p, mask | (1 << (m - 1)))
+						ret && (push!(answer, p), return true)
+					end
+				end
 			end
 		end
+		false
 	end
-
-	println(@. length(p))
-	println(prod(@. length(p)))
+	for p ∈ p₀[1] 
+		ret = recurse(p, p, 1)
+		ret && (push!(answer, p), break)
+	end
+	sum(answer)
 end
 
-solve(10 ^ 3, 10 ^ 4 - 1)
+open("answer.txt", "w") do af
+	write(af, string(solve()))
+end

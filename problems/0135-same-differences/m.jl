061ab6058ffe2fcb1c33692c525cfe881df02036
Dustin Miao <67491820+dustin-miao@users.noreply.github.com>
2023-06-15 22:24:22 -0700

    added problem 135

---
+++ problems/0135-same-differences/m.jl
@@ -0,0 +1,16 @@
+function solve()
+	ν = 10^6
+	μ = zeros(Int16, ν)
+	for n ∈ 1:ν
+		for r ∈ n÷3+1:ν
+			k = (3r-n)*(n+r)
+			if k > ν break end
+			μ[k] += 1
+		end
+	end
+	count(x->(x==10), μ)
+end
+
+open("answer.txt", "w") do af
+	write(af, string(solve()))
+end

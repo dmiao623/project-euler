c499291daa1bfdb87234654f1d6c51949542578a
Dustin Miao <67491820+dustin-miao@users.noreply.github.com>
2023-06-28 11:33:39 -0700

    added problem 201

--- problems/0201-subsets-with-a-unique-sum/m.jl
+++ problems/0201-subsets-with-a-unique-sum/m.jl
@@ -8,7 +8,7 @@ function solve()
 		b = sum(j^2 for j ∈ 1:i-1)
 		for j ∈ max(1,51-(101-i)):51, k ∈ 1:b+1
 			f[i,j,k] = f[i-1,j,k]+(k-1>=w&&j>1 ? f[i-1,j-1,k-w] : 0)
-		end 
+		end
 	end
 	sum(f[101,51,i]==1 ? i-1 : 0 for i ∈ 2:κ)
 end

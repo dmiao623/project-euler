373ed8ae3d276fde5c4456e81013f10b7aa08089
Dustin Miao <67491820+dustin-miao@users.noreply.github.com>
2023-07-02 06:42:45 -0700

    added problem 197

--- problems/0197-investigating-the-behavior-of-a-recursively-defined-sequence/m.jl
+++ problems/0197-investigating-the-behavior-of-a-recursively-defined-sequence/m.jl
@@ -1,1 +1,9 @@
-₀
+f(x) = floor(2 ^ (30.403243784 - x ^ 2)) * 10 ^ -9
+
+function solve()
+	x::Float64 = -1
+	for i ∈ 1:3000 x = f(x) end
+	x + f(x)
+end
+
+open("answer.txt", "w") do af write(af, string(solve())) end

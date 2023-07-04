164dc29e643d9ed41a95353d7c53afb7dae0e840
Dustin Miao <67491820+dustin-miao@users.noreply.github.com>
2023-07-03 21:48:15 -0400

    added problem 710

---
+++ problems/0710-one-million-members/m.jl
@@ -0,0 +1,22 @@
+using Mods
+
+function solve()
+	n = 10^7
+	f = zeros(Mod{10^6}, n+1)
+	g = zeros(Mod{10^6}, n+1)
+	f[1] = f[2] = f[3] = g[3] = 1
+	σf₁, σf₂ = f[2], f[1] + f[3]
+	σg₁, σg₂ = g[2], g[1] + g[3]
+	@assert 0 == Mod{10^6}(0)
+	for i ∈ 3:n
+		f[i+1] = σf₁ - (i<4 ? 0 : f[i-4+1]) + 1
+		σf₁ += f[i+1]
+		σf₁, σf₂ = σf₂, σf₁
+		g[i+1] = σg₁ + (i<4 ? 0 : f[i-4+1])
+		σg₁ += g[i+1]
+		σg₁, σg₂ = σg₂, σg₁
+		if i > 3 && g[i+1] == 0 return i end
+	end
+end
+
+@time open("answer.txt", "w") do af write(af, string(solve())) end
